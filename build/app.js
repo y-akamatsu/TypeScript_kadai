6ba19e7a5b2aa8aba208c9675b2f044e689c34f4
y-akamatsu <ak1985tk@gmail.com>
2019-04-09 22:26:07 +0900

    kadai_7

--- build/app.js
+++ build/app.js
@@ -1,9 +1,55 @@
-function getArrayString(str1, str2, str3) {
-    return [str1, str2, str3];
+function calc(a, b, type) {
+    switch (type) {
+        case CalcType.Add:
+            return a + b;
+        case CalcType.Sub:
+            return a - b;
+        case CalcType.Multi:
+            return a * b;
+        case CalcType.Div:
+            return a / b;
+    }
 }
-console.log(getArrayString('俺は', '明日', '勉強する'));
-console.log(getArrayString('明日', '気温', '下がるってよ'));
-console.log(getArrayString('明日', '東京', '雪降るらしい'));
+var CalcType;
+(function (CalcType) {
+    CalcType[CalcType["Add"] = 0] = "Add";
+    CalcType[CalcType["Sub"] = 1] = "Sub";
+    CalcType[CalcType["Multi"] = 2] = "Multi";
+    CalcType[CalcType["Div"] = 3] = "Div";
+})(CalcType || (CalcType = {}));
+const a = 10;
+const b = 2;
+console.log("Add", calc(a, b, CalcType.Add));
+console.log("sub", calc(a, b, CalcType.Sub));
+console.log("Muliti", calc(a, b, CalcType.Multi));
+console.log("Div", calc(a, b, CalcType.Div));
+// function calc(a: number, b: number, type: CalcType): number {
+//   switch (type) {
+//     case CalcType.Add:
+//       return a + b
+//     case CalcType.Sub:
+//       return a - b
+//     case CalcType.Multi:
+//       return a * b
+//     case CalcType.Div:
+//       return a / b
+//   }
+// }
+// enum CalcType {
+//   Add, Sub, Multi, Div
+// }
+// const a = 10;
+// const b = 2;
+// console.log("Add", calc(a,b, CalcType.Add))
+// console.log("sub", calc(a,b, CalcType.Sub))
+// console.log("Multi", calc(a,b, CalcType.Multi))
+// console.log("Div", calc(a, b, CalcType.Div))
+// function getArrayString(str1: string, str2: string, str3: string): string[] {
+//   return [str1, str2, str3]
+// }
+// console.log(getArrayString('俺は', '明日', '勉強する'))
+// console.log(getArrayString('明日', '気温', '下がるってよ'))
+// console.log(getArrayString('明日', '東京', '雪降るらしい'))
 // function mergeString(str1: string, str2: string, str3: string): string {
 //   return str1 + str2 + str3
 // }
