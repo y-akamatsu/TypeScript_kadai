e825f39778c32c4df8268d181a5cb303f04b7678
y-akamatsu <ak1985tk@gmail.com>
2019-04-09 20:04:18 +0900

    kadai_3

--- build/app.js
+++ build/app.js
@@ -1,31 +1,23 @@
-function getHelloWithData() {
-    const data = new Date(10);
-    const hours = data.getHours();
-    if (5 >= hours && hours < 12) {
-        return "おはようございます";
+function showHundred1() {
+    let result = "";
+    for (let i = 0; i <= 100; i++) {
+        result += `${i}`;
     }
-    else if (12 >= hours && hours < 19) {
-        return "こんにちわ";
-    }
-    else if (19 >= hours && hours < 1) {
-        return "こんばんわ";
-    }
-    else {
-        return "夜更かしさん";
-    }
+    console.log(result);
 }
-console.log(getHelloWithData());
-// function getHelloWithDate(): string {
-//   const date = new Date()
-//   const hours = date.getHours()
-//   if (5 <= hours && hours < 12) {
-//     return 'おはようございます'
-//   } else if (12 <= hours && hours < 18) {
-//     return 'こんにちは'
-//   } else if (18 <= hours && hours < 24) {
-//     return 'こんばんは'
+console.log(showHundred1());
+// function getHelloWithData(): string {
+//   const data = new Date(10) //ここに時間を入力
+//   const hours = data.getHours()
+//   if (5 >= hours && hours < 12) {
+//     return "おはようございます"
+//   } else if (12 >= hours && hours < 19) {
+//     return "こんにちわ"
+//   } else if (19 >= hours && hours < 1) {
+//     return "こんばんわ"
 //   } else {
-//     return '夜更かしさん'
+//     return "夜更かしさん"
 //   }
 // }
+// console.log(getHelloWithData())
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,33 @@
-function getHelloWithData(): string {
-  const data = new Date(10) //ここに時間を入力
-  const hours = data.getHours()
-  if (5 >= hours && hours < 12) {
-    return "おはようございます"
-  } else if (12 >= hours && hours < 19) {
-    return "こんにちわ"
-  } else if (19 >= hours && hours < 1) {
-    return "こんばんわ"
-  } else {
-    return "夜更かしさん"
+function showHundred1(): void {
+  let result: string = ""
+  for (let i = 0; i <= 100; i++) {
+    result += `${i} `
   }
+  console.log(result)
 }
-console.log(getHelloWithData())
+
+console.log(showHundred1())
 
 
-// function getHelloWithDate(): string {
-//   const date = new Date()
-//   const hours = date.getHours()
-//   if (5 <= hours && hours < 12) {
-//     return 'おはようございます'
-//   } else if (12 <= hours && hours < 18) {
-//     return 'こんにちは'
-//   } else if (18 <= hours && hours < 24) {
-//     return 'こんばんは'
+
+
+
+
+
+
+
+// function getHelloWithData(): string {
+//   const data = new Date(10) //ここに時間を入力
+//   const hours = data.getHours()
+//   if (5 >= hours && hours < 12) {
+//     return "おはようございます"
+//   } else if (12 >= hours && hours < 19) {
+//     return "こんにちわ"
+//   } else if (19 >= hours && hours < 1) {
+//     return "こんばんわ"
 //   } else {
-//     return '夜更かしさん'
+//     return "夜更かしさん"
 //   }
 // }
+// console.log(getHelloWithData())
+
